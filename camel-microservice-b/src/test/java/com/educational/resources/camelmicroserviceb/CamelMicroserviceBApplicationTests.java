7f8a00367a65f951c458624aee1aa22260d73965
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-18 12:23:31 +0100

    Start two microservice projects

---
+++ camel-microservice-b/src/test/java/com/educational/resources/camelmicroserviceb/CamelMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educational.resources.camelmicroserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
