73e7d52e4558d5a7c5fe0e65da7290a166b5c929
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-31 17:11:47 +0100

    Add currencyExchangeProcessor and Transformer

--- camel-microservice-b/src/main/java/com/educational/resources/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
+++ camel-microservice-b/src/main/java/com/educational/resources/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
@@ -3,6 +3,9 @@ package com.educational.resources.camelmicroserviceb.routes;
 import com.educational.resources.camelmicroserviceb.CurrencyExchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -10,14 +13,47 @@ import java.math.BigDecimal;
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder {
 
+    @Autowired
+    private MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
+
+    @Autowired
+    private MyCurrencyExchangeTransformer myCurrencyExchangeTransformer;
+
     @Override
     public void configure() throws Exception {
         /*from("activemq:my-queue")
                 .to("log:received-message-from-active-mq");
         */
         from("activemq:my-queue")
-                .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+                .unmarshal()
+                .json(JsonLibrary.Jackson, CurrencyExchange.class)
+                .bean(myCurrencyExchangeProcessor)
+                .bean(myCurrencyExchangeTransformer)
                 .to("log:received-message-from-active-mq");
     }
 }
 
+@Component
+class MyCurrencyExchangeProcessor {
+
+    Logger logger = LoggerFactory.getLogger(ActiveMqReceiverRouter.class);
+
+    public CurrencyExchange processMessage(CurrencyExchange currencyExchange) {
+
+        logger.info("Do some processing with currencyExchange.getConversionMultiple()",
+                currencyExchange.getConversionMultiple());
+
+        return currencyExchange;
+    }
+}
+
+@Component
+class MyCurrencyExchangeTransformer {
+
+    public CurrencyExchange processMessage(CurrencyExchange currencyExchange) {
+        currencyExchange.setConversionMultiple(
+                currencyExchange.getConversionMultiple().multiply(BigDecimal.TEN));
+
+        return currencyExchange;
+    }
+}
